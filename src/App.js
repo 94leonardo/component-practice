c4f2c455dc90a585b5ef0ada3f9c09eefe3451af
94leonardo <lassoleonardo56@gmail.com>
2024-05-22 21:32:19 -0500

    cambios nuevos22052024

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-import MyComponentes from './componentes/MyComponentes';
-import { SegundoComponente } from './componentes/SegundoComponente';
-import { ThirdComponentes } from './componentes/ThirdComponentes';
+import logo from "./logo.svg";
+import "./App.css";
+import MyComponentes from "./componentes/MyComponentes";
+import { SegundoComponente } from "./componentes/SegundoComponente";
+import { ThirdComponentes } from "./componentes/ThirdComponentes";
+import { useState } from "react";
+import { Child } from "./componentes/Child";
+import { CuartoComponente } from "./componentes/CuartoComponete";
 
 function App() {
+  const [name, setName] = useState("LEONARDO");
+  const [message, setMessage] = useState(" ");
+  const addMessage = (message) => {
+    console.log(message);
+    setMessage(message);
+  };
 
   const medicalRecord = {
     height: "1.60",
     bloodGroup: "RH A+",
-    allergies: "NONE"
-  }
-
-
+    allergies: "NONE",
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Estructura inicial del proyecto y limpia.
-        </p>
+        <p>Estructura inicial del proyecto y limpia.</p>
+
+        <CuartoComponente />
+
+        <h2>Mensaje del hijo</h2>
+        <p>{message}</p>
+        <Child name={name} setName={setName} addMessage={addMessage} />
         <SegundoComponente />
         <ThirdComponentes
           name="leonardo"
@@ -28,7 +39,6 @@ function App() {
           card={medicalRecord}
         />
         <MyComponentes />
-
       </header>
     </div>
   );
--- src/componentes/ThirdComponentes.jsx
+++ src/componentes/ThirdComponentes.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-export const ThirdComponentes = (props) => {
-    console.log(props)
-    return (
-        <div>
-            <h2>Comunicacion entre componentes</h2>
-            <ul>
-                <li>{props.name}</li>
-                <li>{props.lastname}</li>
-                <li>{props.card.allergies}</li>
-                <li>{props.card.bloodGroup}</li>
+export const ThirdComponentes = ({ name, lastname, card }) => {
+  return (
+    <div>
+      <h2>Comunicacion entre componentes</h2>
+      <ul>
+        <li>{name}</li>
+        <li>{lastname}</li>
+        <li>{card.height}</li>
+      </ul>
+    </div>
+  );
+};
 
-            </ul>
-
-        </div>
-    )
-}
+ThirdComponentes.propTypes = {
+  name: PropTypes.string,
+  lastName: PropTypes.string,
+  card: PropTypes.object,
+};
